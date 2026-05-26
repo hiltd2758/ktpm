60fbad65ac575322ef01d0f68f9bd8a2a9412ab7
Tran Dong Hil <151639006+hiltd2758@users.noreply.github.com>
2026-05-27 00:16:40 +0700

    EHC-1: update AdminDTO

--- web/src/main/java/com/e_health_care/web/admin/dto/AdminDTO.java
+++ web/src/main/java/com/e_health_care/web/admin/dto/AdminDTO.java
@@ -1,13 +1,13 @@
-package com.e_health_care.web.admin.dto;
+    package com.e_health_care.web.admin.dto;
 
-import lombok.Data;
+    import lombok.Data;
 
-@Data
-public class AdminDTO {
-    public String email;
-    public String password;
-    private String role = "ROLE_ADMIN";
-    public String getUppercase_role() {
-        return this.role.toUpperCase();
+    @Data
+    public class AdminDTO {
+        public String email;
+        public String password;
+        private String role = "ROLE_ADMIN";
+        public String getUppercase_role() {
+            return this.role.toUpperCase();
+        }
     }
-}
