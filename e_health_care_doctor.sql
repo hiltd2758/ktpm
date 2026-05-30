9a5595b160507fed8287f475d803835f5f72fcb8
Knuwlottie <huynhlekhanhu.kg@gmail.com>
2026-05-30 14:58:10 +0700

    test: add T8 Patient Profile collection

--- e_health_care_doctor.sql
+++ e_health_care_doctor.sql
@@ -34,7 +34,7 @@ CREATE TABLE `doctor` (
   `field` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
